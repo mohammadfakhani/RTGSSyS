86cf8919ea6436f72248abb1cffc55b8113f3f6c
Mohamad Fakhani <predator_mf1@hotmail.com>
2020-06-20 16:39:21 +0300

    .class!!

--- src/main/java/com/RTGS/OrderMessageListener.java
+++ src/main/java/com/RTGS/OrderMessageListener.java
@@ -1,5 +1,9 @@
 package com.RTGS;
 
+import com.RTGS.Settlement.Chaque;
+import com.RTGS.Settlement.settledChecks.SettledChaque;
+import com.RTGS.security.users.RTGSUser;
+import org.hibernate.annotations.Check;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,21 +15,21 @@ public class OrderMessageListener {
     static final Logger logger = LoggerFactory.getLogger(OrderMessageListener.class);
 
     @RabbitListener(queues = "CheckQ2")
-    public void processOrderCheck(Object object) {
-        System.out.println("check recieved with info : " + object);
+    public void processOrderCheck(Chaque chaque) {
+        System.out.println("check recieved with info : " + chaque.toString());
 
-        logger.info("Order Received: " + object);
+        logger.info("Order Received: " + chaque);
     }
     @RabbitListener(queues = "RTGSUserQ")
-    public void processOrderUsers(Object object) {
-        System.out.println("check recieved with info : " + object);
+    public void processOrderUsers(RTGSUser rtgsUser) {
+        System.out.println("check recieved with info : " + rtgsUser.toString());
 
-        logger.info("Order Received: " + object);
+        logger.info("Order Received: " + rtgsUser);
     }
     @RabbitListener(queues = "SettledChaqueQ")
-    public void processOrderSettledCheck(Object object) {
-        System.out.println("check recieved with info : " + object);
+    public void processOrderSettledCheck(SettledChaque settledChaque) {
+        System.out.println("check recieved with info : " + settledChaque.toString());
 
-        logger.info("Order Received: " + object);
+        logger.info("Order Received: " + settledChaque);
     }
 }
